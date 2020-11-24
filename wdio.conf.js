45cb6413988d43bb9419df703c4900f8f33e2027
Harman Cheema <harmancheema07@gmail.com>
2020-11-24 11:40:07 -0500

    updated allure reporter

--- wdio.conf.js
+++ wdio.conf.js
@@ -25,8 +25,8 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        // './test/specs/**/*.js',
-        './test/specs/herokuapp/dragAndDrop.spec.js'
+        './test/specs/**/*.js',
+        // './test/specs/herokuapp/dragAndDrop.spec.js'
     ],
     // Patterns to exclude.
     exclude: [
@@ -73,7 +73,7 @@ exports.config = {
             },
             // to run chrome headless the following flags are required
             // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-            // args: ['--headless', '--disable-gpu'],
+            args: ['--headless', '--disable-gpu'],
         }
     }],
     //
@@ -305,6 +305,24 @@ exports.config = {
      */
     onComplete: function(exitCode, config, capabilities, results) {
         rmdir(downloadDir)
+        const reportError = new Error('Could not generate Allure report')
+        const generation = allure(['generate', 'allure-results', '--clean'])
+        return new Promise((resolve, reject) => {
+            const generationTimeout = setTimeout(
+                () => reject(reportError),
+                5000)
+
+            generation.on('exit', function(exitCode) {
+                clearTimeout(generationTimeout)
+
+                if (exitCode !== 0) {
+                    return reject(reportError)
+                }
+
+                console.log('Allure report successfully generated')
+                resolve()
+            })
+        })
     },
     /**
     * Gets executed when a refresh happens.
